1f277a61c910def48969187b1bab5cfe09b66c5d
Carine <carine.fattal19@imperial.ac.uk>
2019-10-21 21:16:13 +0100

    elif

--- Week-3/numberGuessingGame.py
+++ Week-3/numberGuessingGame.py
@@ -2,9 +2,11 @@ guess = input ('Pick a number between 1 and 10:')
 guess = int(guess)
 import random
 surprise = random.randint(1,10)
+print (surprise)
 if guess == surprise:
     print('True')
-else:
+elif guess != surprise:
+    print ('You were close, yet so far')
     print ('Try again')
     guess = input()
     guess = int(guess)
