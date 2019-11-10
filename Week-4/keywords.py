e6f576dd9a84cb6b18f88ab2651490597def94f5
CarineFattal <carine.fattal19@imperial.ac.uk>
2019-11-10 23:04:51 +0000

    try

--- Week-4/keywords.py
+++ Week-4/keywords.py
@@ -14,43 +14,67 @@ with default values.
 # it returns 'Hello and welcome to <place>'
 # if both user_name and place are provided
 # it returns 'Hello, <user_name>, and welcome to <place>
-def welcome_message(*user_name, **place):
-    
-    if place and user_name:
-        return ('Hello, '+ user_name + ',' + 'and welcome to' + place)
-    elif place or user_name:
-        place = str(place)
-        return ('Hello, '+ user_name + ',' + 'and welcome to ' + place)
-    
+def welcome_message(user_name='', place=''):
+    '''
+    function1
+    '''
+    final_output = ''
+    if user_name and place:
+        final_output = 'Hello, ' + user_name + ', and welcome to ' + place
+    elif user_name:
+        final_output = 'Hello, ' + user_name + ', and welcome' + place
+    elif place:
+        final_output = 'Hello' + user_name + ' and welcome to ' + place
     else:
-        return 'Hello and welcome'
-
-    
-    
-    
+        final_output = 'Hello' + user_name + ' and welcome' + place
+    return final_output
 
 # Create a function called list_average()
-# without using any libraries to do the maths for you 
-# (the point of this exercise is to practice interacting 
+# without using any libraries to do the maths for you
+# (the point of this exercise is to practice interacting
 # with lists)
 # the first argument is a list of numbers
 # the second optional keyword arguemt is called avg_type
 # if nothing for avg_type provided, return the mean of the list
-# if avg_type='mode', return the mode of the list 
+# if avg_type='mode', return the mode of the list
 # (return list of all modes if there is a tie between multiple values)
 # if avg_type='mean', return the mean of the list
 # if avg_type='median', return the median of this list
 
 
 
-def list_average(argument1=[1,2,3], *avg_type):
-    if avg_type == 'mode':
-        for i in argument1:
-            if i == i+1:
-                return i 
-    elif avg_type == 'mean':
-        return sum(argument1)/len(argument1)
-    elif avg_type == 'median':
-        return int(sum(argument1)/len(argument1))
-    else:
-        return sum(argument1)/len(argument1)
+def list_average(argument1, avg_type="mean"):
+    '''
+    function2
+    '''
+
+    if avg_type == "median":
+        argument1.sort()
+        length = len(argument1)
+        if argument1 == []:
+            median = None
+        if length % 2 == 0:
+            median1 = argument1[length//2]
+            median2 = argument1[(length//2) - 1]
+            median = (median1 + median2)//2
+        if length % 2 != 0:
+            median = argument1[length//2]
+        return median
+    if avg_type == "mode":
+        from collections import Counter
+        dictionary = dict(Counter(argument1))
+        maximum1 = max(dictionary, key=dictionary.get)
+        argument1.reverse()
+        dictionary = dict(Counter(argument1))
+        maximum2 = max(dictionary, key=dictionary.get)
+        if maximum1 == maximum2:
+            answer = [maximum1]
+        else:
+            answer = [maximum1, maximum2]
+        return answer
+    if avg_type:
+        if len(argument1) != 0:
+            output = sum(argument1)/len(argument1)
+        else:
+            output = 0
+        return output
--- Week-4/structures.py
+++ Week-4/structures.py
@@ -1,37 +1,49 @@
 '''
 structures.py
 
-Simple functions performing operations on basic Python data structures.  
+Simple functions performing operations on basic Python data structures.
 '''
 
 # Lists
 
 # write a function that returns a list containig the first and the last element
-# of "the_list". 
+# of "the_list".
 def first_and_last(the_list):
+    '''
+    function 1
+    '''
     return [the_list[0], the_list[-1]]
 
 
 
 # write a function that returns part of "the_list" between indices given by the
 # second and third parameter, respectively. The returned part should be in
-# reverse order than in the original "the_list". 
+# reverse order than in the original "the_list".
 # If "end" is greater then "beginning" or any og the indices is out of the
-# list, raise a "ValueError" exception. 
+# list, raise a "ValueError" exception.
 def part_reverse(the_list, beginning, end):
-    if end > beginning:
-        raise ValueError
-    list = the_list[beginning: end]
-    return list.sort(reverse=True)
-
+    '''
+    function2
+    '''
+    try:
+        the_list = the_list[beginning:end]
+        the_list.reverse()
+        return the_list
+    except:
+        if end > beginning:
+            raise ValueError
 
 # write a function that at the "index" of "the_list" inserts three times the
 # same value. For example if the_list = [0,1,2,3,4] and index = 3 the function
-# will return [0,1,2,3,3,3,4]. 
+# will return [0,1,2,3,3,3,4].
 def repeat_at_index(the_list, index):
-    for i in the_list:
-        return the_list.insert(i)
-    
+    '''
+    function3
+    '''
+    the_list.insert(index, index)
+    the_list.insert(index, index)
+    return the_list
+
 
 
 # Strings
@@ -39,17 +51,28 @@ def repeat_at_index(the_list, index):
 # write a function that checks whether the word is a palindrome, i.e. it reads
 # the same forward and backwards
 def palindrome_word(word):
-    if  word == word.reverse:
-        return word
+    '''
+    function4
+    '''
+    word = word.casefold()
+    rev_word = reversed(word)
+    return bool(list(word) == list(rev_word))
 
 # write a function that checks whether the sentence is a palindrome, i.e. it
 # read the same forward and backward. Ignore all spaces and other characters
 # like fullstops, commas, etc. Also do not consider whether the letter is
-# capital or not. 
+# capital or not.
 def palindrome_sentence(sentence):
-    s = sentence.split()
-    if s == s.reverse:
-        return s
+    '''
+    function5
+    '''
+    sentence.lower()
+    sentence.split()
+    sentence.replace(" ", "")
+    sentence = sentence.casefold()
+    rev_sentence = reversed(sentence)
+    return bool(list(sentence) == list(rev_sentence))
+
 
 # write a function that concatenates two sentences. First the function checks
 # whether the sentence meets the following criteria: it starts with a capital
@@ -57,15 +80,22 @@ def palindrome_sentence(sentence):
 # Keep in mind, that the sentence might have whitespace at the beginning or at
 # the end.  The concatenated sentence must have no white space at the beginning
 # or at the end and the must be exactly one space after the end of the first
-# sentence. 
-def concatenate_sentences(sentenece1, sentenece2):
-    sentenece1 = sentenece1.split()
-    sentenece1 = sentenece1.upper(0)
-    sentenece2 = sentenece2.split()
-    if sentenece2[-1] == '.' or '!' or '?':
-        return sentenece1 + ' ' + sentenece2
-    else:
-        print('Your sentenece must end with a full stop, question mqrk or qn exlamation point')
+# sentence.
+def concatenate_sentences(sentence1, sentence2):
+    '''
+    function6
+    '''
+    final_output = ''
+    sentence1 = sentence1.split()
+    sentence1 = sentence1[0].upper
+    sentence2 = sentence2.split()
+    sentence2 = sentence2[0].upper
+    group = ['.', '!', '?']
+    if sentence2[-1] in group and sentence1[-1] in group:
+        final_output = str(sentence1) + ' ' + str(sentence2)
+    if sentence2[-1] not in group and sentence1[-1] not in group:
+        final_output = 'Your sentence must end with a full stop, question mark or an !'
+    return final_output
 
 
 # Dictionaries
@@ -73,24 +103,24 @@ def concatenate_sentences(sentenece1, sentenece2):
 # write a function that checks whether there is a record with given key in the
 # dictionary. Return True or False.
 def index_exists(dictionary, key):
-    if key in dictionary:
-        return True
-    else:
-        return False
+    '''
+    function7
+    '''
+    return bool(key in dictionary.keys())
 
 # write a function which checks whether given value is stored in the
 # dictionary. Return True or False.
 def value_exists(dictionary, value):
-    if value in dictionary:
-        return True
-    else:
-        return False
-
+    '''
+    function8
+    '''
+    return bool(value in dictionary.values())
 
 # write a function that returns a new dictionary which contains all the values
 # from dictionary1 and dictionary2.
 def merge_dictionaries(dictionary1, dictionary2):
-    new_dictionary = dictionary1.update(dictionary2)
-    return new_dictionary
-
-
+    '''
+    function9
+    '''
+    dictionary1.update(dictionary2)
+    return dictionary1
