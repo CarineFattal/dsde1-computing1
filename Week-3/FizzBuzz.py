fceba31811a3ad37d2d47cf1faae365faed8880c
CarineFattal <carine.fattal19@imperial.ac.uk>
2019-11-11 21:36:44 +0000

    try

--- Week-3/FizzBuzz.py
+++ Week-3/FizzBuzz.py
@@ -1,10 +1,11 @@
-a=0
-for a in range(101):
-    if a%3 == 0 and a%5 == 0:
-        print ('fizz-buzz')
-    elif a%3 == 0 and a%5 != 0:
-        print ('fizz')
-    elif a%3 != 0 and a%5 == 0:
-        print ('buzz')
+'''FizzBuzz exercice'''
+first_value = 0
+for first_value in range(101):
+    if first_value%3 == 0 and first_value%5 == 0:
+        print('fizz-buzz')
+    elif first_value%3 == 0 and first_value != 0:
+        print('fizz')
+    elif first_value%3 != 0 and first_value%5 == 0:
+        print('buzz')
     else:
-        print (a)
+        print(first_value)
